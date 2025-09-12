702d9363f600a426254e177d365d3ced54093a8f
antonioMonaco99 <151932314+antonioMonaco99@users.noreply.github.com>
2025-09-12 16:39:32 +0200

    Update write_effect_intro.js

--- js/write_effect_intro.js
+++ js/write_effect_intro.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     new TypeIt('#write_effect', {
-        strings: ["Computer Science student"],
+        strings: ["Computer Science Engineering student"],
         speed: 50,
         waitUntilVisible: true
     }).go();
