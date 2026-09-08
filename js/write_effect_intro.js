93a8141a2e4bd311b4aa06d5bd4959039bc5a95f
antonioMonaco99 <151932314+antonioMonaco99@users.noreply.github.com>
2026-09-08 11:16:58 +0200

    Rifatto il sito con design moderno

--- js/write_effect_intro.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    new TypeIt('#write_effect', {
-        strings: ["Computer Science Engineering student"],
-        speed: 50,
-        waitUntilVisible: true
-    }).go();
-});
